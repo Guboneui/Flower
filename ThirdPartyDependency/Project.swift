66eaa406659f14ba2ae1e54ab9ce6f8e08f326b4
보니 <73548875+Guboneui@users.noreply.github.com>
2023-09-09 07:37:34 +0900

    [SETTING] `ThirdPartyDependency` Dynamic Framework 생성

---
+++ ThirdPartyDependency/Project.swift
@@ -0,0 +1,21 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 구본의 on 2023/09/09.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import ThirdPartyDependencyPlugin
+
+let projectName: String = "ThirdPartyDependency"
+let iOSTargetVersion: String = "16.0"
+
+let project = Project.makeFrameworkProject(
+  name: projectName,
+  iOSTargetVersion: iOSTargetVersion,
+  dependencies: TargetDependency.ThirdParty.dependency,
+  isDynamic: true,
+  needTestTarget: false
+)
---
+++ ThirdPartyDependency/Resources/a.swift
@@ -0,0 +1,9 @@
+//
+//  a.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 구본의 on 2023/09/09.
+//
+
+import Foundation
+let a = 10
