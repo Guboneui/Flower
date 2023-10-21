e245d97a3656b5bc5120863381ecbfffd8869868
Guboneui <starku2249@naver.com>
2023-10-21 16:31:46 +0900

    [FEAT] `DesignSystem` appImage 추가
    디자인 파일 내 정의 된, 이미지 추가
    
    Co-Authored-By: minneee <86248191+minneee@users.noreply.github.com>
    Co-Authored-By: Donggaem <86482563+Donggaem@users.noreply.github.com>

--- ResourceKit/Sources/UIImage+Extension/UIImage+Extension.swift
+++ ResourceKit/Sources/UIImage+Extension/UIImage+Extension.swift
@@ -9,6 +9,18 @@ import UIKit
 
 public extension UIImage {
   struct AppImage {
-    public static let jjangu = ResourceKitAsset.Image.jjangu.image
+    public static let arrowLeft = ResourceKitAsset.Image.arrowLeft.image
+    public static let arrowRight = ResourceKitAsset.Image.arrowRight.image
+    public static let camera = ResourceKitAsset.Image.camera.image
+    public static let caution = ResourceKitAsset.Image.caution.image
+    public static let checkBoxOff = ResourceKitAsset.Image.checkBoxOff.image
+    public static let checkBoxOn = ResourceKitAsset.Image.checkBoxOn.image
+    public static let delete = ResourceKitAsset.Image.delete.image
+    public static let email = ResourceKitAsset.Image.email.image
+    public static let password = ResourceKitAsset.Image.password.image
+    public static let profile = ResourceKitAsset.Image.profile.image
+    public static let showOff = ResourceKitAsset.Image.showOff.image
+    public static let showOn = ResourceKitAsset.Image.showOn.image
+    public static let xmark = ResourceKitAsset.Image.xmark.image
   }
 }
