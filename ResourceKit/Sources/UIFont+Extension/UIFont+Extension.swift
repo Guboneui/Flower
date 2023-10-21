a41d9aa08e26630998a7f910290235c36427b62b
Guboneui <starku2249@naver.com>
2023-10-22 03:39:49 +0900

    [EDIT] appFont `Bold_10` 추가
    Co-Authored-By: minneee <86248191+minneee@users.noreply.github.com>
    Co-Authored-By: Donggaem <86482563+Donggaem@users.noreply.github.com>

--- ResourceKit/Sources/UIFont+Extension/UIFont+Extension.swift
+++ ResourceKit/Sources/UIFont+Extension/UIFont+Extension.swift
@@ -15,6 +15,7 @@ public extension UIFont {
     public static let Regular_16 = ResourceKitFontFamily.Pretendard.regular.font(size: 16)
     
     // MARK: - Weight 700
+    public static let Bold_10 = ResourceKitFontFamily.Pretendard.bold.font(size: 10)
     public static let Bold_14 = ResourceKitFontFamily.Pretendard.bold.font(size: 14)
     public static let Bold_16 = ResourceKitFontFamily.Pretendard.bold.font(size: 16)
     public static let Bold_20 = ResourceKitFontFamily.Pretendard.bold.font(size: 20)
