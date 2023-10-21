52ef449e36e894658481c1adca5aea40c69aa59c
Guboneui <starku2249@naver.com>
2023-10-21 16:09:06 +0900

    [FEAT] `DesignSystem` AppColor 적용
    디자인 시스템에 적용되어 있는 7가지 색상을 지정
    
    Co-Authored-By: minneee <86248191+minneee@users.noreply.github.com>
    Co-Authored-By: Donggaem <86482563+Donggaem@users.noreply.github.com>

--- ResourceKit/Sources/UIColor+Extension/UIColor+Extension.swift
+++ ResourceKit/Sources/UIColor+Extension/UIColor+Extension.swift
@@ -9,6 +9,17 @@ import UIKit
 
 public extension UIColor {
   struct AppColor {
-    public static let primaryColor = ResourceKitAsset.Color.primaryColor.color
+    // MARK: - Black
+    public static let appBlack = ResourceKitAsset.Color.black.color
+    
+    // MARK: - Grey
+    public static let appGrey100 = ResourceKitAsset.Color.grey100.color
+    public static let appGrey200 = ResourceKitAsset.Color.grey200.color
+    public static let appGrey700 = ResourceKitAsset.Color.grey700.color
+    public static let appGrey800 = ResourceKitAsset.Color.grey800.color
+    
+    // MARK: - Primary
+    public static let appPrimary = ResourceKitAsset.Color.primary.color
+    public static let appPrimaryLight = ResourceKitAsset.Color.primaryLight.color
   }
 }
