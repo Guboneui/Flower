a3df43e8fc971a4967aa27b773716f90c4ef066f
Donggaem <ehdrua465111@naver.com>
2023-10-30 20:41:23 +0900

    [EDIT] comment 수정사항 적용

--- Feature/Login/Sources/LoginViewController.swift
+++ Feature/Login/Sources/LoginViewController.swift
@@ -2,7 +2,7 @@
 //  LoginViewController.swift
 //  Login
 //
-//  Created by 구본의 on 2023/10/17.
+//  Created by 김동겸 on 2023/10/17.
 //
 
 import UIKit
@@ -14,75 +14,76 @@ import SnapKit
 import Then
 
 public final class LoginViewController: UIViewController {
+  
+  private let mainLogoView: UIView = UIView().then {
+    $0.backgroundColor = .AppColor.appGrey70
+  }
+  
+  private let loginLabel: UILabel = UILabel().then {
+    $0.text = "로그인/회원가입"
+    $0.font = .AppFont.Regular_14
+    $0.textColor = .AppColor.appBlack
+    $0.textAlignment = .center
+  }
+  
+  private lazy var stackViewSubViews: [SocialLoginButton] = [
+    naverLoginButton, 
+    appleLoginButton,
+    kakaoLoginButton,
+    emailLoginButton
+  ]
+  
+  private lazy var socialLoginsStackView: UIStackView = UIStackView(
+    arrangedSubviews: stackViewSubViews
+  ).then {
+    $0.axis = .horizontal
+    $0.alignment = .fill
+    $0.distribution = .fillEqually
+    $0.spacing = 24
+  }
+  
+  private let naverLoginButton: SocialLoginButton = SocialLoginButton(.naver)
+  private let appleLoginButton: SocialLoginButton = SocialLoginButton(.apple)
+  private let kakaoLoginButton: SocialLoginButton = SocialLoginButton(.kakao)
+  private let emailLoginButton: SocialLoginButton = SocialLoginButton(.email)
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
     
-    private let mainLogoView: UIView = UIView().then {
-        $0.backgroundColor = .AppColor.appGrey70
+    setupUI()
+    setupViews()
+  }
+}
+
+private extension LoginViewController {
+  
+  func setupUI() {
+    view.backgroundColor = .white
+  }
+  
+  func setupViews() {
+    view.addSubview(mainLogoView)
+    view.addSubview(loginLabel)
+    view.addSubview(socialLoginsStackView)
+    
+    setupConstraints()
+  }
+  
+  func setupConstraints() {
+    mainLogoView.snp.makeConstraints { make in
+      make.width.height.equalTo(100)
+      make.centerX.equalToSuperview()
+      make.bottom.equalTo(loginLabel.snp.top).offset(-38)
     }
     
-    private let loginLabel: UILabel = UILabel().then {
-        $0.text = "로그인/회원가입"
-        $0.font = .AppFont.Regular_14
-        $0.textColor = .AppColor.appBlack
-        $0.textAlignment = .center
+    loginLabel.snp.makeConstraints { make in
+      make.horizontalEdges.equalToSuperview()
+      make.centerY.equalToSuperview() 
     }
     
-    private let socialLoginsStackView: UIStackView = UIStackView().then {
-        $0.axis = .horizontal
-        $0.alignment = .fill
-        $0.distribution = .fillEqually
-        $0.spacing = 24
+    socialLoginsStackView.snp.makeConstraints { make in
+      make.top.equalTo(loginLabel.snp.bottom).offset(28)
+      make.horizontalEdges.equalToSuperview().inset(52)
     }
-    
-    private let naverLoginButton: SocialLoginButton = SocialLoginButton(.naver)
-    private let appleLoginButton: SocialLoginButton = SocialLoginButton(.apple)
-    private let kakaoLoginButton: SocialLoginButton = SocialLoginButton(.kakao)
-    private let emailLoginButton: SocialLoginButton = SocialLoginButton(.email)
-    
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupViews()
-    }
-    
-    private func setupViews() {
-        view.backgroundColor = .white
-        
-        view.addSubview(mainLogoView)
-        view.addSubview(loginLabel)
-        
-        view.addSubview(socialLoginsStackView)
-        
-        socialLoginsStackView.addArrangedSubview(naverLoginButton)
-        socialLoginsStackView.addArrangedSubview(appleLoginButton)
-        socialLoginsStackView.addArrangedSubview(kakaoLoginButton)
-        socialLoginsStackView.addArrangedSubview(emailLoginButton)
-        
-        setupConstraints()
-    }
-    
-    private func setupConstraints() {
-        mainLogoView.snp.makeConstraints { make in
-            make.width.equalTo(100)
-            make.height.equalTo(100)
-            make.top.equalToSuperview().inset(277)
-            make.centerX.equalToSuperview()
-            
-        }
-        
-        loginLabel.snp.makeConstraints { make in
-            make.top.equalTo(mainLogoView.snp.bottom).offset(38)
-            make.leading.equalToSuperview().inset(0)
-            make.trailing.equalToSuperview().inset(0)
-            
-        }
-        
-        socialLoginsStackView.snp.makeConstraints { make in
-            make.top.equalTo(loginLabel.snp.bottom).offset(28)
-            make.bottom.equalToSuperview().inset(338)
-            make.leading.equalToSuperview().inset(52)
-            make.trailing.equalToSuperview().inset(52)
-        }
-        
-    }
-    
+  }
 }
