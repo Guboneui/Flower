e97a5321615ae45b1248670e480a38b770d1f12d
Guboneui <starku2249@naver.com>
2023-11-18 01:35:35 +0900

    [REFACTOR] AppTheme enum 으로 Asset 관리

---
+++ ResourceKit/Sources/AppTheme.swift
@@ -0,0 +1,10 @@
+//
+//  AppTheme.swift
+//  ResourceKit
+//
+//  Created by 구본의 on 2023/11/18.
+//
+
+import Foundation
+
+public enum AppTheme { }
--- ResourceKit/Sources/UIFont+Extension/UIFont+Extension.swift
+++ ResourceKit/Sources/UIFont+Extension/UIFont+Extension.swift
@@ -7,18 +7,31 @@
 
 import UIKit
 
-public extension UIFont {
-  struct AppFont {
-    // MARK: - Weight 400
-    public static let Regular_12 = ResourceKitFontFamily.Pretendard.regular.font(size: 12)
-    public static let Regular_14 = ResourceKitFontFamily.Pretendard.regular.font(size: 14)
-    public static let Regular_16 = ResourceKitFontFamily.Pretendard.regular.font(size: 16)
-    
-    // MARK: - Weight 700
-    public static let Bold_10 = ResourceKitFontFamily.Pretendard.bold.font(size: 10)
-    public static let Bold_14 = ResourceKitFontFamily.Pretendard.bold.font(size: 14)
-    public static let Bold_16 = ResourceKitFontFamily.Pretendard.bold.font(size: 16)
+extension AppTheme {
+	public enum Font {
+		public static let Regular_4 = ResourceKitFontFamily.Pretendard.regular.font(size: 4)
+		public static let Regular_6 = ResourceKitFontFamily.Pretendard.regular.font(size: 6)
+		public static let Regular_8 = ResourceKitFontFamily.Pretendard.regular.font(size: 8)
+		public static let Regular_10 = ResourceKitFontFamily.Pretendard.regular.font(size: 10)
+		public static let Regular_12 = ResourceKitFontFamily.Pretendard.regular.font(size: 12)
+		public static let Regular_14 = ResourceKitFontFamily.Pretendard.regular.font(size: 14)
+		public static let Regular_16 = ResourceKitFontFamily.Pretendard.regular.font(size: 16)
+		public static let Regular_18 = ResourceKitFontFamily.Pretendard.regular.font(size: 18)
+		public static let Regular_20 = ResourceKitFontFamily.Pretendard.regular.font(size: 20)
+		public static let Regular_22 = ResourceKitFontFamily.Pretendard.regular.font(size: 22)
+		public static let Regular_24 = ResourceKitFontFamily.Pretendard.regular.font(size: 24)
+		
+		// MARK: - Weight 700
+		public static let Bold_4 = ResourceKitFontFamily.Pretendard.bold.font(size: 4)
+		public static let Bold_6 = ResourceKitFontFamily.Pretendard.bold.font(size: 6)
+		public static let Bold_8 = ResourceKitFontFamily.Pretendard.bold.font(size: 8)
+		public static let Bold_10 = ResourceKitFontFamily.Pretendard.bold.font(size: 10)
+		public static let Bold_12 = ResourceKitFontFamily.Pretendard.bold.font(size: 12)
+		public static let Bold_14 = ResourceKitFontFamily.Pretendard.bold.font(size: 14)
+		public static let Bold_16 = ResourceKitFontFamily.Pretendard.bold.font(size: 16)
 		public static let Bold_18 = ResourceKitFontFamily.Pretendard.bold.font(size: 18)
-    public static let Bold_20 = ResourceKitFontFamily.Pretendard.bold.font(size: 20)
-  }
+		public static let Bold_20 = ResourceKitFontFamily.Pretendard.bold.font(size: 20)
+		public static let Bold_22 = ResourceKitFontFamily.Pretendard.bold.font(size: 22)
+		public static let Bold_24 = ResourceKitFontFamily.Pretendard.bold.font(size: 24)
+	}
 }
