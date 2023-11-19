e88546a0db278f91551e988e4c6411692ef21d44
Guboneui <starku2249@naver.com>
2023-11-19 15:11:04 +0900

    [FEAT] SearchFilterViewController

---
+++ Feature/SearchFilter/Sources/SearchFilterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchFilterViewController.swift
+//  SearchFilterDemoApp
+//
+//  Created by 구본의 on 2023/11/19.
+//
+
+import UIKit
+
+import DesignSystem
+import ResourceKit
+
+public final class SearchFilterViewController: UIViewController {
+	public override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = AppTheme.Color.primary
+	}
+}
