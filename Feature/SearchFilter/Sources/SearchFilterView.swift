1cace0820fdcd124b2ccdaeb0a14df2aa6ad0aca
Guboneui <starku2249@naver.com>
2023-11-28 01:08:06 +0900

    [REFACTOR] `rootView` 을 loadView 시점에 주입해, ViewController와 UI 코드 분리

---
+++ Feature/SearchFilter/Sources/SearchFilterView.swift
@@ -0,0 +1,188 @@
+//
+//  SearchFilterView.swift
+//  SearchFilter
+//
+//  Created by 구본의 on 2023/11/27.
+//
+
+import UIKit
+
+import DesignSystem
+import ResourceKit
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+final class SearchFilterView: UIView {
+	
+	// MARK: - METRIC
+	private enum Metric {
+		static let guideLineHeight: CGFloat = 1.0
+		
+		static let bottomButtonTopMargin: CGFloat = 12
+		static let bottomButtonHorizontalMargin: CGFloat = 20
+		static let bottomButtonBottomMargin: CGFloat = UIDevice.hasNotch ? -66 : -32
+	}
+	
+	// MARK: - TEXT SET
+	private enum TextSet {
+		static let navigationTitle: String = "필터"
+		static let leftButtonTitle: String = "선택 취소"
+		static let rightButtonTitle: String = "검색"
+	}
+	
+	// MARK: - UI PROPERTY
+	fileprivate let navigationBar: NavigationBar = NavigationBar(
+		.close,
+		title: TextSet.navigationTitle,
+		hasGuideLine: true
+	)
+	
+	private let scrollView: UIScrollView = UIScrollView()
+	
+	private let scrollContainerView: UIView = UIView().then {
+		$0.backgroundColor = AppTheme.Color.grey90
+	}
+	
+	fileprivate let travelSpotSelectorView: TravelSpotSelectorView = .init()
+	fileprivate let travelGroupSelectorView: TravelGroupSelectorView = .init()
+	
+	private let bottomContainerView: UIView = UIView().then {
+		$0.backgroundColor = AppTheme.Color.white
+	}
+	
+	private let bottomContainerGuideLineView: UIView = UIView().then {
+		$0.backgroundColor = AppTheme.Color.grey70
+	}
+	
+	fileprivate let bottomButton: TwoButton = TwoButton(
+		sizeType: .rightLarger,
+		buttonType: (.origin, .primary),
+		leftButtonTitle: TextSet.leftButtonTitle,
+		rightButtonTitle: TextSet.rightButtonTitle
+	)
+	
+	// MARK: - PROPERTY
+	private let disposeBag: DisposeBag = .init()
+	
+	// MARK: - INITIALIZE
+	public override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupViewConfigure()
+		setupViews()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
+
+// MARK: - PRIVATE METHOD
+private extension SearchFilterView {
+	func setupViewConfigure() {
+		backgroundColor = AppTheme.Color.white
+	}
+	
+	func setupViews() {
+		addSubview(navigationBar)
+		addSubview(scrollView)
+		scrollView.addSubview(scrollContainerView)
+		scrollContainerView.addSubview(travelSpotSelectorView)
+		scrollContainerView.addSubview(travelGroupSelectorView)
+		addSubview(bottomContainerView)
+		bottomContainerView.addSubview(bottomContainerGuideLineView)
+		bottomContainerView.addSubview(bottomButton)
+		
+		setupConstraints()
+	}
+	
+	func setupConstraints() {
+		navigationBar.snp.makeConstraints { make in
+			make.top.equalTo(safeAreaLayoutGuide)
+			make.horizontalEdges.equalToSuperview()
+		}
+		
+		scrollView.snp.makeConstraints { make in
+			make.top.equalTo(navigationBar.snp.bottom)
+			make.horizontalEdges.equalToSuperview()
+			make.bottom.equalTo(bottomContainerView.snp.top)
+		}
+		
+		scrollContainerView.snp.makeConstraints { make in
+			make.edges.equalToSuperview()
+			make.width.equalToSuperview()
+			
+			make.height.equalTo(2000)
+		}
+		
+		travelSpotSelectorView.snp.makeConstraints { make in
+			make.top.equalToSuperview().offset(20)
+			make.horizontalEdges.equalToSuperview().inset(20)
+		}
+		
+		travelGroupSelectorView.snp.makeConstraints { make in
+			make.top.equalTo(travelSpotSelectorView.snp.bottom).offset(20)
+			make.horizontalEdges.equalToSuperview().inset(20)
+		}
+		
+		bottomContainerView.snp.makeConstraints { make in
+			make.bottom.equalToSuperview()
+			make.horizontalEdges.equalToSuperview()
+		}
+		
+		bottomContainerGuideLineView.snp.makeConstraints { make in
+			make.top.equalToSuperview()
+			make.horizontalEdges.equalToSuperview()
+			make.height.equalTo(Metric.guideLineHeight)
+		}
+		
+		bottomButton.snp.makeConstraints { make in
+			make.top.equalToSuperview().offset(Metric.bottomButtonTopMargin)
+			make.horizontalEdges.equalToSuperview().inset(Metric.bottomButtonHorizontalMargin)
+			make.bottom.equalToSuperview().offset(Metric.bottomButtonBottomMargin)
+		}
+	}
+}
+
+// MARK: - Reactive Extension
+extension Reactive where Base: SearchFilterView {
+	var didTapNavigationLeftButton: ControlEvent<Void> {
+		let source = base.navigationBar.rx.tapLeftButton.asObservable()
+		return ControlEvent(events: source)
+	}
+	
+	var didTapBottomLeftButton: ControlEvent<Void> {
+		let source = base.bottomButton.rx.tapLeftButton.asObservable()
+		return ControlEvent(events: source)
+	}
+	
+	var didTapBottomRightButton: ControlEvent<Void> {
+		let source = base.bottomButton.rx.tapRightButton.asObservable()
+		return ControlEvent(events: source)
+	}
+	
+	var didTapDecreaseButton: ControlEvent<Void> {
+		let source = base.travelGroupSelectorView.rx.tapDecreaseButton.asObservable()
+		return ControlEvent(events: source)
+	}
+	
+	var didTapIncreaseButton: ControlEvent<Void> {
+		let source = base.travelGroupSelectorView.rx.tapIncreaseButton.asObservable()
+		return ControlEvent(events: source)
+	}
+	
+	var isEnableIncreaseButton: Binder<Bool> {
+		return base.travelGroupSelectorView.rx.increaseButtonEnable
+	}
+	
+	var isEnableDecreaseButton: Binder<Bool> {
+		return base.travelGroupSelectorView.rx.decreaseButtonEnable
+	}
+	
+	var groupCounterValue: Binder<String> {
+		return base.travelGroupSelectorView.rx.counterValue
+	}
+}
--- Feature/SearchFilter/Sources/SearchFilterViewController.swift
+++ Feature/SearchFilter/Sources/SearchFilterViewController.swift
@@ -17,65 +17,36 @@ import Then
 
 public final class SearchFilterViewController: UIViewController, View {
 	
-	// MARK: METRIC
-	private enum Metric {
-		static let guideLineHeight: CGFloat = 1.0
-		
-		static let bottomButtonTopMargin: CGFloat = 12
-		static let bottomButtonHorizontalMargin: CGFloat = 20
-		static let bottomButtonBottomMargin: CGFloat = UIDevice.hasNotch ? -66 : -32
+	private enum Constant {
+		static let minumumGroupCount: Int = 0
+		static let maximumGroupCount: Int = 6
 	}
 	
 	// MARK: - UI PROPERTY
-	private let navigationBar: NavigationBar = NavigationBar(
-		.close,
-		title: "필터",
-		hasGuideLine: true
-	)
-	
-	private let scrollView: UIScrollView = UIScrollView()
-	
-	private let scrollContainerView: UIView = UIView().then {
-		$0.backgroundColor = AppTheme.Color.grey90
-	}
-	
-	private let travelSpotSelectorView: TravelSpotSelectorView = .init()
-	private let travelGroupSelectorView: TravelGroupSelectorView = .init()
-	
-	private let bottomContainerView: UIView = UIView().then {
-		$0.backgroundColor = AppTheme.Color.white
-	}
-	
-	private let bottomContainerGuideLineView: UIView = UIView().then {
-		$0.backgroundColor = AppTheme.Color.grey70
-	}
-	
-	private let bottomButton: TwoButton = TwoButton(
-		sizeType: .rightLarger,
-		buttonType: (.origin, .primary),
-		leftButtonTitle: "선택 취소",
-		rightButtonTitle: "검색"
-	)
+	private let rootView: SearchFilterView = SearchFilterView()
 	
 	// MARK: - PROPERTY
 	public var disposeBag: DisposeBag = .init()
 	
 	// MARK: - LIFE CYCLE
-	public override func viewDidLoad() {
-		super.viewDidLoad()
-		setupViewConfigure()
-		setupViews()
-		setupGestures()
+	public override func loadView() {
+		super.loadView()
+		self.view = rootView
 	}
 	
+	public override func viewDidLoad() {
+		super.viewDidLoad()
+		self.setupGestures()
+	}
+	
+	// MARK: - BINDING
 	public func bind(reactor: SearchFilterViewReactor) {
-		
-		travelGroupSelectorView.rx.tapDecreaseButton
+		rootView.rx.didTapDecreaseButton
 			.map { .didTapDecreaseButton }
 			.bind(to: reactor.action)
 			.disposed(by: disposeBag)
 		
-		travelGroupSelectorView.rx.tapIncreaseButton
+		rootView.rx.didTapIncreaseButton
 			.map { .didTapIncreaseButton }
 			.bind(to: reactor.action)
 			.disposed(by: disposeBag)
@@ -86,106 +57,28 @@ public final class SearchFilterViewController: UIViewController, View {
 		
 		groupCount
 			.map { String($0) }
-			.bind(to: travelGroupSelectorView.rx.counterValue)
+			.bind(to: rootView.rx.groupCounterValue)
 			.disposed(by: disposeBag)
 		
 		groupCount
-			.map { $0 > 0 }
-			.bind(to: travelGroupSelectorView.rx.decreaseButtonEnable)
+			.map { $0 > Constant.minumumGroupCount }
+			.bind(to: rootView.rx.isEnableDecreaseButton)
 			.disposed(by: disposeBag)
 		
 		groupCount
-			.map { $0 < 6 }
-			.bind(to: travelGroupSelectorView.rx.increaseButtonEnable)
+			.map { $0 < Constant.maximumGroupCount }
+			.bind(to: rootView.rx.isEnableIncreaseButton)
 			.disposed(by: disposeBag)
 	}
 }
 
 // MARK: - PRIVATE METHOD
 private extension SearchFilterViewController {
-	func setupViewConfigure() {
-		view.backgroundColor = AppTheme.Color.white
-	}
-	
-	func setupViews() {
-		view.addSubview(navigationBar)
-		view.addSubview(scrollView)
-		scrollView.addSubview(scrollContainerView)
-		scrollContainerView.addSubview(travelSpotSelectorView)
-		scrollContainerView.addSubview(travelGroupSelectorView)
-		view.addSubview(bottomContainerView)
-		bottomContainerView.addSubview(bottomContainerGuideLineView)
-		bottomContainerView.addSubview(bottomButton)
-		
-		setupConstraints()
-	}
-	
-	func setupConstraints() {
-		navigationBar.snp.makeConstraints { make in
-			make.top.equalTo(view.safeAreaLayoutGuide)
-			make.horizontalEdges.equalToSuperview()
-		}
-		
-		scrollView.snp.makeConstraints { make in
-			make.top.equalTo(navigationBar.snp.bottom)
-			make.horizontalEdges.equalToSuperview()
-			make.bottom.equalTo(bottomContainerView.snp.top)
-		}
-		
-		scrollContainerView.snp.makeConstraints { make in
-			make.edges.equalToSuperview()
-			make.width.equalToSuperview()
-			
-			make.height.equalTo(2000)
-		}
-		
-		travelSpotSelectorView.snp.makeConstraints { make in
-			make.top.equalToSuperview().offset(20)
-			make.horizontalEdges.equalToSuperview().inset(20)
-		}
-		
-		travelGroupSelectorView.snp.makeConstraints { make in
-			make.top.equalTo(travelSpotSelectorView.snp.bottom).offset(20)
-			make.horizontalEdges.equalToSuperview().inset(20)
-		}
-		
-		
-		bottomContainerView.snp.makeConstraints { make in
-			make.bottom.equalToSuperview()
-			make.horizontalEdges.equalToSuperview()
-		}
-		
-		bottomContainerGuideLineView.snp.makeConstraints { make in
-			make.top.equalToSuperview()
-			make.horizontalEdges.equalToSuperview()
-			make.height.equalTo(Metric.guideLineHeight)
-		}
-		
-		bottomButton.snp.makeConstraints { make in
-			make.top.equalToSuperview().offset(Metric.bottomButtonTopMargin)
-			make.horizontalEdges.equalToSuperview().inset(Metric.bottomButtonHorizontalMargin)
-			make.bottom.equalToSuperview().offset(Metric.bottomButtonBottomMargin)
-		}
-	}
-	
 	func setupGestures() {
-		navigationBar.rx.tapLeftButton
+		rootView.rx.didTapNavigationLeftButton
 			.bind { [weak self] in
 				guard let self else { return }
 				self.dismiss(animated: true)
-			}.disposed(by: disposeBag)
-		
-		bottomButton.rx.tapLeftButton
-			.bind { [weak self] in
-				guard let self else { return }
-				let prevState = self.travelSpotSelectorView.currentState.value
-				self.travelSpotSelectorView.currentState.accept(!prevState)
-			}.disposed(by: disposeBag)
-		
-		bottomButton.rx.tapRightButton
-			.bind { [weak self] in
-				guard let self else { return }
-				print("검색")
 			}.disposed(by: disposeBag)
 	}
 }
