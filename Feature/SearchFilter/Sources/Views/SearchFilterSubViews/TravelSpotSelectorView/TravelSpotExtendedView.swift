61f1c2b6eb0b6853d390b13581b85c7f9cbc4818
Guboneui <starku2249@naver.com>
2023-11-29 01:34:56 +0900

    [FIX] TravelSpotExtendedView 레거시 코드 삭제

--- Feature/SearchFilter/Sources/Views/SearchFilterSubViews/TravelSpotSelectorView/TravelSpotExtendedView.swift
+++ Feature/SearchFilter/Sources/Views/SearchFilterSubViews/TravelSpotSelectorView/TravelSpotExtendedView.swift
@@ -19,6 +19,8 @@ final class TravelSpotExtendedView: UIView {
 	
 	// MARK: - METRIC
 	private enum Metric {
+		static let radius: CGFloat = 12
+		
 		static let stackViewSpacing: CGFloat = 24
 		static let stackViewTopMargin: CGFloat = 24
 		static let stackViewHorizontalMargin: CGFloat = 24
@@ -33,16 +35,22 @@ final class TravelSpotExtendedView: UIView {
 		static let popularSpotCollectionViewHeight: CGFloat = 56
 	}
 	
+	private enum TextSet {
+		static let titleLabelText: String = "게스트하우스 위치를 알려주세요"
+		static let searchLabelText: String = "위치 검색"
+		static let popularSearchLabelText: String = "인기 검색어"
+	}
+	
 	// MARK: - UI PROPERTY
 	private let titleLabel: UILabel = UILabel().then {
-		$0.text = "게스트하우스 위치를 알려주세요"
+		$0.text = TextSet.titleLabelText
 		$0.font = AppTheme.Font.Bold_20
 		$0.textColor = AppTheme.Color.black
 	}
 	
-	private let locationSearchContainerView: UIView = UIView().then {
+	private let locationSearchContainerButton: UIButton = UIButton().then {
 		$0.backgroundColor = AppTheme.Color.grey90
-		$0.makeCornerRadius(12)
+		$0.makeCornerRadius(Metric.radius)
 	}
 	
 	private let searchImageView: UIImageView = UIImageView().then {
@@ -51,13 +59,13 @@ final class TravelSpotExtendedView: UIView {
 	}
 	
 	private let searchLabel: UILabel = UILabel().then {
-		$0.text = "위치 검색"
+		$0.text = TextSet.searchLabelText
 		$0.font = AppTheme.Font.Regular_12
 		$0.textColor = AppTheme.Color.black
 	}
 	
 	private let popularSearchLabel: UILabel = UILabel().then {
-		$0.text = "인기 검색어"
+		$0.text = TextSet.popularSearchLabelText
 		$0.font = AppTheme.Font.Regular_12
 		$0.textColor = AppTheme.Color.black
 	}
@@ -69,57 +77,48 @@ final class TravelSpotExtendedView: UIView {
 	private lazy var stackView: UIStackView = UIStackView(
 		arrangedSubviews: [
 			titleLabel,
-			locationSearchContainerView,
+			locationSearchContainerButton,
 			popularSearchLabel
 		]).then {
 			$0.axis = .vertical
 			$0.spacing = Metric.stackViewSpacing
 		}
 	
-	private let locationLabel: UILabel = UILabel().then {
-		$0.text = "위치"
-		$0.font = AppTheme.Font.Regular_12
-		$0.textColor = AppTheme.Color.black
-	}
-	
-	private let selectedLocationLabel: UILabel = UILabel().then {
-		$0.text = "제주특별자치도 서귀포시"
-		$0.font = AppTheme.Font.Regular_12
-		$0.textColor = AppTheme.Color.black
-		$0.textAlignment = .right
-	}
-	
-	public var currentState: BehaviorRelay<Bool> = .init(value: true)
 	private let disposeBag: DisposeBag = .init()
+	
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		setupConfigure()
-		setupBindings()
-		
+		setupSubViews()
 	}
 	
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
+}
+
+// MARK: - PRIVATE METHOD
+private extension TravelSpotExtendedView {
+	func setupConfigure() {
+		backgroundColor = AppTheme.Color.white
+	}
 	
-	public func setExtendableState() {
-		
-		subviews.forEach {
-//			$0.alpha = 0.0
-			$0.removeFromSuperview()
-		}
-//		
+	func setupSubViews() {
 		addSubview(stackView)
-		locationSearchContainerView.addSubview(searchImageView)
-		locationSearchContainerView.addSubview(searchLabel)
+		locationSearchContainerButton.addSubview(searchImageView)
+		locationSearchContainerButton.addSubview(searchLabel)
 		addSubview(popularSpotCollectionView)
 		
+		setupConstraints()
+	}
+	
+	func setupConstraints() {
 		stackView.snp.makeConstraints { make in
 			make.top.equalToSuperview().offset(Metric.stackViewTopMargin)
 			make.horizontalEdges.equalToSuperview().inset(Metric.subViewHorizontalMargin)
 		}
 		
-		locationSearchContainerView.snp.makeConstraints { make in
+		locationSearchContainerButton.snp.makeConstraints { make in
 			make.height.equalTo(Metric.locationContainerViewHeight)
 		}
 		
@@ -141,49 +140,5 @@ final class TravelSpotExtendedView: UIView {
 			make.horizontalEdges.equalToSuperview()
 			make.height.equalTo(Metric.popularSpotCollectionViewHeight)
 		}
-		
-	}
-	
-	public func setTestState() {
-		
-		subviews.forEach {
-			$0.removeFromSuperview()
-		}
-		
-		addSubview(locationLabel)
-		locationLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-		addSubview(selectedLocationLabel)
-		
-		
-		self.locationLabel.snp.makeConstraints { make in
-			make.leading.equalToSuperview().offset(32)
-			make.verticalEdges.equalToSuperview().inset(24)
-		}
-		
-		self.selectedLocationLabel.snp.makeConstraints { make in
-			make.leading.equalTo(self.locationLabel.snp.trailing).offset(8)
-			make.trailing.equalToSuperview().offset(-32)
-			make.centerY.equalToSuperview()
-		}
-		
-	}
-}
-
-// MARK: - PRIVATE METHOD
-private extension TravelSpotExtendedView {
-	func setupConfigure() {
-		backgroundColor = AppTheme.Color.white
-	}
-	
-	func setupBindings() {
-		currentState.asDriver()
-			.drive { [weak self] state in
-				guard let self else { return }
-				if state {
-					self.setExtendableState()
-				} else {
-					self.setTestState()
-				}
-			}.disposed(by: disposeBag)
 	}
 }
