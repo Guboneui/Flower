d80c95f562d517b551eb361258cb6d76a7c0a6bf
Donggaem <ehdrua465111@naver.com>
2023-12-07 00:36:30 +0900

    [FEAT] 이메일 회원가입 비밀번호 입력화면 로직 구현

---
+++ Feature/Login/Sources/Email/Signup/EmailSignupPW/EmailSignupPWViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  EmailSignupPWViewModel.swift
+//  Login
+//
+//  Created by 김동겸 on 12/6/23.
+//
+
+import Foundation
+
+import RxRelay
+import RxSwift
+
+public final class EmailSignupPWViewModel {
+	var pwRelay: BehaviorRelay<String> = .init(value: "")
+	var pwCheckRelay: BehaviorRelay<String> = .init(value: "")
+	
+	var pwBool: BehaviorRelay<Bool?> = .init(value: nil)
+	var pwCheckBool: BehaviorRelay<Bool?> = .init(value: nil)
+	
+	let pwRegex: String = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,20}"
+	
+	func isValiedPW() {
+		if NSPredicate(format: "SELF MATCHES %@", pwRegex).evaluate(with: pwRelay.value) {
+			pwBool.accept(true)
+		} else {
+			pwBool.accept(false)
+		}
+	}
+	
+	func isValiedPWCheck() {
+		if pwCheckRelay.value == pwRelay.value {
+			pwCheckBool.accept(true)
+		} else {
+			pwCheckBool.accept(false)
+		}
+	}
+}
