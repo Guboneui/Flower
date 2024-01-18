56f7b49fe21f4b3030acbc91822ce21a419393ed
Donggaem <ehdrua465111@naver.com>
2024-01-18 13:57:36 +0900

    [REFACTOR] 들여쓰기 수정

--- UtilityKit/Sources/Extensions/String+Extension/String+Extension.swift
+++ UtilityKit/Sources/Extensions/String+Extension/String+Extension.swift
@@ -9,15 +9,15 @@ import UIKit
 
 public extension String {
 	///입력된 String의 길이를 측정하여 CGRect값으로 반환하는 함수
-		func getEstimatedFrame(with font: UIFont) -> CGRect {
-				let size = CGSize(width: UIScreen.main.bounds.width * 2/3, height: 1000)
-				let optionss = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
-				let estimatedFrame = NSString(string: self).boundingRect(
-					with: size,
-					options: optionss,
-					attributes: [.font: font],
-					context: nil
-				)
-				return estimatedFrame
-		}
+	func getEstimatedFrame(with font: UIFont) -> CGRect {
+		let size = CGSize(width: UIScreen.main.bounds.width * 2/3, height: 1000)
+		let optionss = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+		let estimatedFrame = NSString(string: self).boundingRect(
+			with: size,
+			options: optionss,
+			attributes: [.font: font],
+			context: nil
+		)
+		return estimatedFrame
+	}
 }
