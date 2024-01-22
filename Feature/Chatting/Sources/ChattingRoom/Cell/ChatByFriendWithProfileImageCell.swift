d526c61580d648ca51c6e4ddeb29c5d462ba1aa3
Donggaem <ehdrua465111@naver.com>
2024-01-22 22:17:10 +0900

    [FEAT] 채팅방UI구성

---
+++ Feature/Chatting/Sources/ChattingRoom/Cell/ChatByFriendWithProfileImageCell.swift
@@ -0,0 +1,155 @@
+//
+//  ChatByFriendWithProfileImageCell.swift
+//  Chatting
+//
+//  Created by 김동겸 on 1/15/24.
+//
+
+import UIKit
+
+import DesignSystem
+import ResourceKit
+import UtilityKit
+
+import SnapKit
+import Then
+
+public class ChatByFriendWithProfileImageCell: UICollectionViewCell {
+	// MARK: - METRIC
+	private enum Metric {
+		static let profileImageViewCornerRadius: CGFloat = 18
+		static let profileImageViewSize: CGFloat = 36
+		static let profileImageViewTopMargin: CGFloat = 8
+		static let profileImageViewLeftMargin: CGFloat = 12
+		
+		static let userNameLabelLeftMargin: CGFloat = 8
+		
+		static let messageBubbleViewCornerRadius: CGFloat = 12
+		static let messageBubbleViewAddWidthtMargin: CGFloat = 20
+		static let messageBubbleViewAddHeightMargin: CGFloat = 16
+		static let messageBubbleViewTopMargin: CGFloat = 4
+		static let messageBubbleViewBottomMargin: CGFloat = 4
+		
+		static let messageLabelNumberOfLines: Int = 0
+		static let messageLabelVerticalMargin: CGFloat = 8
+		static let messageLabelHorizontalMargin: CGFloat = 8
+		
+		static let timeLabelLeftMargin: CGFloat = 4
+	}
+	
+	// MARK: - TextSet
+	private enum TextSet {
+		static let identifier: String = "ChatByFriendWithProfileImageCell"
+	}
+	
+	// MARK: - UI Property
+	private let profileImageView: UIImageView = UIImageView().then {
+		$0.backgroundColor = .orange
+		$0.makeCornerRadius(Metric.profileImageViewCornerRadius)
+	}
+	
+	public let userNameLabel: UILabel = UILabel().then {
+		$0.font = AppTheme.Font.Regular_12
+		$0.textColor = AppTheme.Color.black
+	}
+	
+	private let messageBubbleView: UIView = UIView().then {
+		$0.backgroundColor = AppTheme.Color.secondary
+		$0.makeCornerRadius(Metric.messageBubbleViewCornerRadius)
+	}
+	
+	public let messageLabel: UILabel = UILabel().then {
+		$0.font = AppTheme.Font.Regular_12
+		$0.textColor = AppTheme.Color.black
+		$0.numberOfLines = Metric.messageLabelNumberOfLines
+		$0.lineBreakMode = .byCharWrapping
+	}
+	
+	private let timeLabel: UILabel = UILabel().then {
+		$0.font = AppTheme.Font.Regular_10
+		$0.textColor = AppTheme.Color.black
+	}
+	
+	// MARK: - Property
+	static let identifier = TextSet.identifier
+	
+	// MARK: - Iitialize
+	public override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupConfigures()
+		setupViews()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - PUBLIC METHOD
+	public func remakeCellConstraints() {
+		messageBubbleView.snp.remakeConstraints { make in
+			guard let messageText = messageLabel.text else { return }
+			let estimatedFrame = messageText.getEstimatedFrame(with: messageLabel.font)
+			
+			make.width.equalTo(estimatedFrame.width + Metric.messageBubbleViewAddWidthtMargin)
+			make.height.equalTo(estimatedFrame.height + Metric.messageBubbleViewAddHeightMargin)
+			make.top.equalTo(userNameLabel.snp.bottom).offset(Metric.messageBubbleViewTopMargin)
+			make.leading.equalTo(userNameLabel.snp.leading)
+			make.bottom.equalToSuperview().inset(Metric.messageBubbleViewBottomMargin)
+		}
+	}
+}
+
+// MARK: - PRIVATE METHOD
+private extension ChatByFriendWithProfileImageCell {
+	func setupConfigures() {
+		self.backgroundColor = AppTheme.Color.grey90
+	}
+	
+	func setupViews() {
+		self.addSubview(profileImageView)
+		self.addSubview(userNameLabel)
+		self.addSubview(messageBubbleView)
+		messageBubbleView.addSubview(messageLabel)
+		self.addSubview(timeLabel)
+		
+		setupConstraints()
+	}
+	
+	func setupConstraints() {
+		profileImageView.snp.makeConstraints { make in
+			make.size.equalTo(Metric.profileImageViewSize)
+			make.top.equalToSuperview().inset(Metric.profileImageViewTopMargin)
+			make.leading.equalToSuperview().inset(Metric.profileImageViewLeftMargin)
+		}
+		
+		userNameLabel.snp.makeConstraints { make in
+			make.top.equalTo(profileImageView.snp.top)
+			make.leading.equalTo(profileImageView.snp.trailing).offset(Metric.userNameLabelLeftMargin)
+		}
+		
+		messageBubbleView.snp.makeConstraints { make in
+			guard let messageText = messageLabel.text else { return }
+			let estimatedFrame = messageText.getEstimatedFrame(with: messageLabel.font)
+			
+			make.width.equalTo(estimatedFrame.width + Metric.messageBubbleViewAddWidthtMargin)
+			make.height.equalTo(estimatedFrame.height + Metric.messageBubbleViewAddHeightMargin)
+			make.top.equalTo(userNameLabel.snp.bottom).offset(Metric.messageBubbleViewTopMargin)
+			make.leading.equalTo(userNameLabel.snp.leading)
+			make.bottom.equalToSuperview().inset(Metric.messageBubbleViewBottomMargin)
+		}
+		
+		messageLabel.snp.makeConstraints { make in
+			make.verticalEdges.equalToSuperview().inset(Metric.messageLabelVerticalMargin)
+			make.horizontalEdges.equalToSuperview().inset(Metric.messageLabelHorizontalMargin)
+		}
+		
+		timeLabel.snp.makeConstraints { make in
+			make.bottom.equalTo(messageBubbleView.snp.bottom)
+			make.leading.equalTo(messageBubbleView.snp.trailing).offset(Metric.timeLabelLeftMargin)
+		}
+	}
+	
+	func setupBinds() {
+		
+	}
+}
---
+++ Feature/Chatting/Sources/ChattingRoom/View/ChattingRoomView.swift
@@ -0,0 +1,309 @@
+//
+//  ChattingView.swift
+//  ChattingDemoApp
+//
+//  Created by 구본의 on 2023/12/28.
+//
+
+import UIKit
+
+import DesignSystem
+import ResourceKit
+import UtilityKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+final class ChattingRoomView: UIView {
+	// MARK: - METRIC
+	private enum Metric {
+		static let animateWithDuration: TimeInterval = 0.5
+		
+		static let bottomStackViewSpcing: CGFloat = 12
+		
+		static let chattingRoomBottomViewHeight: CGFloat = 56
+		
+		static let bottomStackViewVerticalMargin: CGFloat = 8
+		static let bottomStackViewHorizontalMargin: CGFloat = 12
+		
+		static let addPhotoMenuButtonViewWidth: CGFloat = 24
+		static let addPhotoMenuButtonBottomMargin: CGFloat = 8
+		
+		static let addPhotoMenuViewWidth: CGFloat = 60
+		static let cameraButtonBottomMargin: CGFloat = 8
+		static let galleryButtonLeftMargin: CGFloat = 12
+		
+		static let messageTextViewCornerRadius: CGFloat = 20
+		static let inputMessageTextViewVerticalMargin: CGFloat = 8
+		static let inputMessageTextViewLeftMargin: CGFloat = 16
+		static let inputMessageTextViewRightMargin: CGFloat = 45
+		static let sendMessageButtonBottomMargin: CGFloat = 8
+		static let sendMessageButtonRightMargin: CGFloat = 12
+	}
+	
+	// MARK: - UI Property
+	private let chattingRoomCollectionViewLayout: UICollectionViewCompositionalLayout = {
+		let itemSize = NSCollectionLayoutSize(
+			widthDimension: .fractionalWidth(1),
+			heightDimension: .estimated(1)
+		)
+		
+		let item = NSCollectionLayoutItem(layoutSize: itemSize)
+		
+		let groupSize = NSCollectionLayoutSize(
+			widthDimension: .fractionalWidth(1),
+			heightDimension: .estimated(30)
+		)
+		
+		let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+		
+		let section = NSCollectionLayoutSection(group: group)
+		
+		return UICollectionViewCompositionalLayout(section: section)
+	}()
+	
+	public lazy var chattingRoomCollectionView: UICollectionView = UICollectionView(
+		frame: .zero,
+		collectionViewLayout: chattingRoomCollectionViewLayout
+	).then {
+		$0.backgroundColor = AppTheme.Color.grey90
+		$0.register(
+			ChatByFriendWithProfileImageCell.self,
+			forCellWithReuseIdentifier: ChatByFriendWithProfileImageCell.identifier
+		)
+		$0.register(
+			ChatByFriendCell.self,
+			forCellWithReuseIdentifier: ChatByFriendCell.identifier
+		)
+		$0.register(
+			ChatByOwnerCell.self,
+			forCellWithReuseIdentifier: ChatByOwnerCell.identifier
+		)
+	}
+	
+	private let chattingRoomBottomView: UIView = UIView().then {
+		$0.backgroundColor = AppTheme.Color.white
+	}
+	
+	private let addPhotoMenuButtonView: UIView = UIView().then {
+		$0.backgroundColor = AppTheme.Color.white
+	}
+	
+	fileprivate let addPhotoMenuButton: UIButton = UIButton().then {
+		$0.setImage(AppTheme.Image.plus, for: .normal)
+		$0.tintColor = AppTheme.Color.black
+	}
+	
+	private let addPhotoMenuView: UIView = UIView().then {
+		$0.backgroundColor = AppTheme.Color.white
+	}
+	
+	private let cameraButton: UIButton = UIButton().then {
+		$0.setImage(AppTheme.Image.useCamera, for: .normal)
+		$0.tintColor = AppTheme.Color.black
+	}
+	
+	private let galleryButton: UIButton = UIButton().then {
+		$0.setImage(AppTheme.Image.selectPhoto, for: .normal)
+		$0.tintColor = AppTheme.Color.black
+	}
+	
+	private let messageTextFieldView: UIView = UIView().then {
+		$0.backgroundColor = AppTheme.Color.grey90
+		$0.makeCornerRadius(Metric.messageTextViewCornerRadius)
+		$0.makeBorder()
+	}
+	
+	public let inputMessageTextView: UITextView = UITextView().then {
+		$0.font = AppTheme.Font.Regular_12
+		$0.textColor = AppTheme.Color.black
+		$0.backgroundColor = AppTheme.Color.grey90
+		$0.isScrollEnabled = false
+	}
+	
+	fileprivate let sendMessageButton: UIButton = UIButton().then {
+		$0.setImage(AppTheme.Image.send, for: .normal)
+	}
+	
+	private lazy var bottomStackView: UIStackView = UIStackView(
+		arrangedSubviews: [
+			addPhotoMenuButtonView,
+			addPhotoMenuView,
+			messageTextFieldView
+		]).then {
+			$0.axis = .horizontal
+			$0.distribution = .fill
+			$0.spacing = Metric.bottomStackViewSpcing
+		}
+	
+	// MARK: - Iitialize
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupConfigures()
+		setupViews()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
+
+extension ChattingRoomView: Viewable {
+	func setupConfigures() {
+		backgroundColor = AppTheme.Color.white
+	}
+	
+	func setupViews() {
+		addSubview(chattingRoomCollectionView)
+		addSubview(chattingRoomBottomView)
+		
+		chattingRoomBottomView.addSubview(bottomStackView)
+		
+		addPhotoMenuButtonView.addSubview(addPhotoMenuButton)
+		
+		addPhotoMenuView.addSubview(cameraButton)
+		addPhotoMenuView.addSubview(galleryButton)
+		
+		messageTextFieldView.addSubview(inputMessageTextView)
+		messageTextFieldView.addSubview(sendMessageButton)
+		
+		setupConstraints()
+	}
+	
+	func setupConstraints() {
+		chattingRoomCollectionView.snp.makeConstraints { make in
+			make.top.equalToSuperview()
+			make.horizontalEdges.equalToSuperview()
+			make.bottom.equalTo(chattingRoomBottomView.snp.top)
+		}
+		
+		chattingRoomBottomView.snp.makeConstraints { make in
+			make.height.equalTo(Metric.chattingRoomBottomViewHeight)
+			make.horizontalEdges.equalToSuperview()
+			make.bottom.equalTo(keyboardLayoutGuide.snp.top)
+		}
+		
+		bottomStackView.snp.makeConstraints { make in
+			make.verticalEdges.equalToSuperview().inset(Metric.bottomStackViewVerticalMargin)
+			make.horizontalEdges.equalToSuperview().inset(Metric.bottomStackViewHorizontalMargin)
+		}
+		
+		addPhotoMenuButtonView.snp.makeConstraints { make in
+			make.width.equalTo(Metric.addPhotoMenuButtonViewWidth)
+		}
+		
+		addPhotoMenuButton.snp.makeConstraints { make in
+			make.centerX.equalToSuperview()
+			make.bottom.equalToSuperview().inset(Metric.addPhotoMenuButtonBottomMargin)
+		}
+		
+		addPhotoMenuView.snp.makeConstraints { make in
+			make.width.equalTo(Metric.addPhotoMenuViewWidth)
+		}
+		
+		cameraButton.snp.makeConstraints { make in
+			make.bottom.equalToSuperview().inset(Metric.cameraButtonBottomMargin)
+			make.leading.equalToSuperview()
+		}
+		
+		galleryButton.snp.makeConstraints { make in
+			make.centerY.equalTo(cameraButton)
+			make.leading.equalTo(cameraButton.snp.trailing).offset(Metric.galleryButtonLeftMargin)
+		}
+		
+		inputMessageTextView.snp.makeConstraints { make in
+			make.verticalEdges.equalToSuperview().inset(Metric.inputMessageTextViewVerticalMargin)
+			make.leading.equalToSuperview().inset(Metric.inputMessageTextViewLeftMargin)
+			make.trailing.equalToSuperview().inset(Metric.inputMessageTextViewRightMargin)
+		}
+		
+		sendMessageButton.snp.makeConstraints { make in
+			make.bottom.equalToSuperview().inset(Metric.sendMessageButtonBottomMargin)
+			make.trailing.equalToSuperview().inset(Metric.sendMessageButtonRightMargin)
+		}
+	}
+	
+	func setupBinds() {
+		
+	}
+	
+	func addPhotoMenuButtonAnimation(with isMenuOpen: Bool) {
+		UIView.animate(withDuration: Metric.animateWithDuration, animations: {
+			self.addPhotoMenuView.alpha = isMenuOpen ? 0.0 : 1.0
+			self.addPhotoMenuView.isHidden = isMenuOpen
+			self.addPhotoMenuButton.transform = isMenuOpen ?
+				.identity : CGAffineTransform(rotationAngle: .pi / 4 )
+		})
+	}
+	
+	func sendMessageButtonAnimation(with isHidden: Bool) {
+		UIView.animate(withDuration: Metric.animateWithDuration, animations: {
+			self.sendMessageButton.isHidden = isHidden
+			self.sendMessageButton.alpha = isHidden ? 0.0 : 1.0
+			
+			if isHidden {
+				self.inputMessageTextView.text = ""
+			}
+		})
+	}
+	
+	func sizingBottomView() {
+		guard let font = self.inputMessageTextView.font else { return }
+		let estimatedFrame = self.inputMessageTextView.text.getEstimatedFrame(with: font)
+		
+		if estimatedFrame.height > 15 && estimatedFrame.height < 58 {
+			self.chattingRoomBottomView.snp.updateConstraints { make in
+				make.height.equalTo(estimatedFrame.height + Metric.chattingRoomBottomViewHeight)
+			}
+		} else if estimatedFrame.height > 57 {
+			self.inputMessageTextView.isScrollEnabled = true
+		} else {
+			returnOriginalSizingBottomView()
+		}
+	}
+	
+	func returnOriginalSizingBottomView() {
+		self.chattingRoomBottomView.snp.updateConstraints { make in
+			make.height.equalTo(Metric.chattingRoomBottomViewHeight)
+		}
+	}
+	
+	func scollingBottom() {
+		self.chattingRoomCollectionView.setContentOffset(
+			CGPoint(
+				x: 0,
+				y: self.chattingRoomCollectionView.contentSize.height
+				- self.chattingRoomCollectionView.bounds.height
+			),
+			animated: true
+		)
+	}
+}
+
+extension Reactive where Base: ChattingRoomView {
+	var didTapSendMessageButton: ControlEvent<Void> {
+		let source = base.sendMessageButton.rx.touchHandler()
+		return ControlEvent(events: source)
+	}
+	
+	var didTapAddPhotoMenuButton: ControlEvent<Void> {
+		let source = base.addPhotoMenuButton.rx.touchHandler()
+		return ControlEvent(events: source)
+	}
+	
+	var didTapChattingRoomCollectionView: ControlEvent<Void> {
+		let source = base.chattingRoomCollectionView.rx.tapGesture().when(.recognized).map { _ in }
+		return ControlEvent(events: source)
+	}
+	
+	var inputMessageTextViewCurrentText: ControlProperty<String?> {
+		return base.inputMessageTextView.rx.text
+	}
+	
+	var didChangeInputMessageTextView: ControlEvent<()> {
+		let source = base.inputMessageTextView.rx.didChange
+		return ControlEvent(events: source)
+	}
+}
