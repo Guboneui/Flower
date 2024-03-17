f9b25b4f67c118ab112f117831b238de66258b46
Donggaem <ehdrua465111@naver.com>
2024-03-17 23:13:15 +0900

    [FEAT] ChatRepository 생성

---
+++ Domain/ChattingDomain/Sources/Interfaces/ChatRepositoryInterface.swift
@@ -0,0 +1,24 @@
+//
+//  ChatRepositoryInterface.swift
+//  ChattingDomain
+//
+//  Created by 김동겸 on 3/1/24.
+//
+
+import Foundation
+
+import ChattingEntity
+
+import RxSwift
+
+public protocol ChatRepositoryInterface {
+	var receivedSoketDataSubject: PublishSubject<ReceiveMessage> { get set }
+	
+	func fetchChannelListAPI() -> Single<ChannelListResponse>
+	func fetchChannelMemberListAPI(channelID: String) -> Single<ChannelMemberListResponse>
+	
+	func connectSoket()
+	func disconnentSoket()
+	func sendToSoket(message: SendMessage)
+	func receiveFromSoket()
+}
