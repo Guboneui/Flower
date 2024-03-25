87007f419349bda6b89804172f988facd7c47b00
Guboneui <starku2249@naver.com>
2024-03-26 01:58:43 +0900

    [SETTING] Service/NetworkHelper

---
+++ Service/NetworkHelper/Sources/Utility/NetworkProvider.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkProvider.swift
+//  NetworkHelper
+//
+//  Created by 구본의 on 3/26/24.
+//
+
+import Foundation
+
+import Moya
+
+public final class NetworkProvider<Target: TargetType>: MoyaProvider<Target> {
+	public override init(
+		endpointClosure: @escaping MoyaProvider<Target>.EndpointClosure = MoyaProvider.defaultEndpointMapping,
+		requestClosure: @escaping MoyaProvider<Target>.RequestClosure = MoyaProvider<Target>.defaultRequestMapping,
+		stubClosure: @escaping MoyaProvider<Target>.StubClosure = MoyaProvider.neverStub,
+		callbackQueue: DispatchQueue? = nil,
+		session: Session = MoyaProvider<Target>.defaultAlamofireSession(),
+		plugins: [PluginType] = [NetworkLogPlugin()],
+		trackInflights: Bool = false
+	) {
+		super.init(
+			endpointClosure: endpointClosure,
+			requestClosure: requestClosure,
+			stubClosure: stubClosure,
+			callbackQueue: callbackQueue,
+			session: session,
+			plugins: plugins,
+			trackInflights: trackInflights
+		)
+	}
+}
