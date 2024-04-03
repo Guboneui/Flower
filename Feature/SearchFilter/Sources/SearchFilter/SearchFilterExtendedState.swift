226a2fa2e958a394ce8d47673b50d90d9b257e37
Guboneui <starku2249@naver.com>
2024-04-04 00:18:25 +0900

    [SETTING] SearchFilter 기존 코드 복구

---
+++ Feature/SearchFilter/Sources/SearchFilter/SearchFilterExtendedState.swift
@@ -0,0 +1,14 @@
+//
+//  SearchFilterExtendedState.swift
+//  SearchFilter
+//
+//  Created by 구본의 on 4/4/24.
+//
+
+import Foundation
+
+public enum SearchFilterExtendedState {
+	case travelSpot
+	case travelDate
+	case travelGroup
+}
