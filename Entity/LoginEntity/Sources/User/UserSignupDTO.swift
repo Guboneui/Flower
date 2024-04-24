6048cbb4b55a8547d8047168d4f272539bda1f2e
Donggaem <ehdrua465111@naver.com>
2024-04-24 15:11:10 +0900

    [REFACTOR] 이메일로그인파라미터명 변경

--- Entity/LoginEntity/Sources/User/UserSignupDTO.swift
+++ Entity/LoginEntity/Sources/User/UserSignupDTO.swift
@@ -10,10 +10,10 @@ import Foundation
 public struct UserSignupDTO {
 	public var email: String?
 	public var password: String?
-	public var userName: String?
-	public var userNickName: String?
+	public var name: String?
+	public var nickname: String?
 	public var birth: String?
-	public var profileImg: Data?
+	public var avatar: Data?
 	public var phoneNum: String?
 	
 	public init(
@@ -27,10 +27,10 @@ public struct UserSignupDTO {
 	) {
 		self.email = email
 		self.password = password
-		self.userName = userName
-		self.userNickName = userNickName
+		self.name = userName
+		self.nickname = userNickName
 		self.birth = birth
-		self.profileImg = profileImg
+		self.avatar = profileImg
 		self.phoneNum = phoneNum
 	}
 }
