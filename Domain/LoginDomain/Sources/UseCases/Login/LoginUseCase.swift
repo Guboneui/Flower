4c0c51c8818bfa18b4a19a00d3271ec1029b8b26
Donggaem <ehdrua465111@naver.com>
2024-05-27 19:04:35 +0900

    [REFACTOR] Login UseCase return 키워드 생략

--- Domain/LoginDomain/Sources/UseCases/Login/LoginUseCase.swift
+++ Domain/LoginDomain/Sources/UseCases/Login/LoginUseCase.swift
@@ -21,22 +21,22 @@ public final class LoginUseCase: LoginUseCaseInterface {
 	}
 	
 	public func fetchEmailLogin(email: String, password: String) -> Single<EmailLoginResponse> {
-		return loginRepository.fetchEmailLoginAPI(email: email, password: password)
+		loginRepository.fetchEmailLoginAPI(email: email, password: password)
 	}
 	
 	public func fetchEmailConfirm(email: String) -> Single<EmptyResponse> {
-		return loginRepository.fetchEmailConfirmAPI(email: email)
+		loginRepository.fetchEmailConfirmAPI(email: email)
 	}
 	
 	public func fetchEmailCodeSent(email: String) -> Single<EmptyResponse> {
-		return loginRepository.fetchEmailCodeSentAPI(email: email)
+		loginRepository.fetchEmailCodeSentAPI(email: email)
 	}
 	
 	public func fetchEmailCodeConfirm(email: String, code: String) -> Single<EmptyResponse> {
-		return loginRepository.fetchEmailCodeConfirmAPI(email: email, code: code)
+		loginRepository.fetchEmailCodeConfirmAPI(email: email, code: code)
 	}
 	
 	public func fetchEmailSignup(userSignupDTO: UserSignupDTO) -> Single<EmptyResponse> {
-		return loginRepository.fetchEmailSignupAPI(userSignupDTO: userSignupDTO)
+		loginRepository.fetchEmailSignupAPI(userSignupDTO: userSignupDTO)
 	}
 }
